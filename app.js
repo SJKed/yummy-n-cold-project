495ceca9ef6842335aff2f1fe9f4b8e6a24af7e6
SJKed <simonjurstedtskolan@gmail.com>
2022-03-30 22:23:52 +0200

    gitignore (for real) app.js functionality and a ER diagram

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const { PromisedDatabase } = require('promised-sqlite3')
+
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+
+app.listen(8080)
