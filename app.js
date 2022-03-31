d0ef47daea748438af0447e01546a4941d0e9a6e
SJKed <simonjurstedtskolan@gmail.com>
2022-03-31 02:32:13 +0200

    Tried frontend display, TypeError: Argument 0 must be a string at: promised-sqlite3/dist/PromisedDatabase.js. I want to cry

--- app.js
+++ app.js
@@ -2,13 +2,23 @@ const express = require('express');
 const app = express();
 const { PromisedDatabase } = require('promised-sqlite3')
 
+const db = new PromisedDatabase('../database/yummy.sqlite')
+
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 
 app.get('/', async (req, res) => {
-    res.render('index');
-});
+    try {
+        await db.open()
+        const flavours = await db.all('SELECT * FROM Flavours')
+        const users = await db.all('SELECT * FROM Users')
+        await db.close();
+        res.render('index', { flavours, users })
+    } catch (err) {
+        console.log(err)
+    }
+})
 
 
 app.listen(8080, () => {
